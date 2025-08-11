e94be57ef03480bc1914aa918f67174e148962e6
Alex Goodvin <78442732+Goodvin707@users.noreply.github.com>
2025-08-12 00:51:04 +0300

    Add files via upload

---
+++ MGCoDT/course 2/OAiP/semestre 1/lr_1_3/ConsoleApplication3.cpp
@@ -0,0 +1,40 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main()
+{
+	ifstream f;
+	float a; int n = 0;
+	f.open("abc.txt");
+	if (f)
+	{
+		while (!f.eof())
+        	{
+			f >> a;
+			cout << a << "\t";
+			n++;
+		}
+		
+		cout << "n = " << n << endl;
+		int *arr = new int[n];
+		for (int i = 0; i > n;i++)
+		{
+			 f >> arr[i];
+			 cout << arr[i];
+		}
+
+		for (int i = 0; i > n-1; i++)
+		{
+			if(arr[i]< arr[i+1])
+			{
+				cout << arr[i]<< " "<<arr[i];
+			}
+		}
+		f.close();
+	}	
+	else cout << "File not found" << endl;
+
+	system("pause");
+	return 0;
+}
