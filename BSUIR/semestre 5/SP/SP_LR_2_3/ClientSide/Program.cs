d92cfeb714343759a06b1f56cc3ed670b8c43810
Alex Goodvin <minsktf@gmail.com>
2025-10-04 12:52:46 +0300

    semestre 5 fixes

---
+++ BSUIR/semestre 5/SP/SP_LR_2_3/ClientSide/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.IO.Pipes;
+using System.IO;
+using System.Diagnostics;
+
+namespace ClientSide
+{
+    class Worker
+    {
+        public Worker() { }
+        public void Client()
+        {
+            NamedPipeClientStream clp = new NamedPipeClientStream(".", "char", PipeDirection.InOut);
+            clp.Connect();
+            StreamWriter sw = new StreamWriter(clp);
+            StreamReader sr = new StreamReader(clp);
+            sw.AutoFlush = true;
+            sw.WriteLine($"Клиент с процессом: {Process.GetCurrentProcess().Id}");
+
+            while (true)
+            {
+                string s = sr.ReadLine();
+                Console.WriteLine(s);
+                Console.Write("Введите строку, которая будет отправлена на сервер: ");
+                sw.WriteLine(Console.ReadLine());
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Worker objWorker = new Worker();
+            Thread objTrh = new Thread(objWorker.Client);
+            objTrh.Start();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($"Запущен клиент с процессом: {Process.GetCurrentProcess().Id}");
+        }
+    }
+}
---
+++ BSUIR/semestre 5/SP/SP_LR_2_3/ServerSide/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServerSide
+{
+    class Server
+    {
+        static Semaphore sem = new Semaphore(2, 2);
+        public Server() { }
+        public void ServerIPX()
+        {
+            NamedPipeServerStream sps = new NamedPipeServerStream("char", PipeDirection.InOut, 3);
+
+            sps.WaitForConnection();
+            sem.WaitOne();
+            
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("\nКо мне подключился клиент. Мой поток: " + Thread.CurrentThread.ManagedThreadId);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            StreamWriter sw = new StreamWriter(sps);
+            StreamReader sr = new StreamReader(sps);
+            sw.AutoFlush = true;
+            
+            while(true)
+            {
+                Console.WriteLine(sr.ReadLine());
+                Console.WriteLine("Выберите пункт меню");
+                Console.WriteLine("1. Отправить на клиент строку\n2. Отключить клиента\n");
+                switch (Console.ReadKey().Key)
+                {
+                    case ConsoleKey.D1:
+                        Console.Write("\nВведите строку, которая будет отправлена на клиент: ");
+                        sw.WriteLine(Console.ReadLine());
+                        break;
+                    case ConsoleKey.D2:
+                        Console.WriteLine("\n" + sem.Release());
+                        break;
+                }
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+
+            Server objServ = new Server();
+            Thread[] mas = new Thread[3];
+            for (int i = 0; i < mas.Length; i++)
+            {
+                mas[i] = new Thread(objServ.ServerIPX);
+                mas[i].Start();
+                Console.WriteLine("Запущен сервер в потоке: " + mas[i].ManagedThreadId);
+            }
+        }
+    }
+}
